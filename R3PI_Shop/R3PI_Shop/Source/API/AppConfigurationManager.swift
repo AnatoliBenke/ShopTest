65a003fb71f0c162ef158bf7ef7b9ad2c463c323
Anatoli Benke <anatoli.benke@accedo.tv>
2017-07-02 18:44:18 +0800

    - CleanUp ConfigurationManager

--- R3PI_Shop/R3PI_Shop/Source/API/AppConfigurationManager.swift
+++ R3PI_Shop/R3PI_Shop/Source/API/AppConfigurationManager.swift
@@ -40,43 +40,41 @@ class AppConfigurationManager: NSObject {
     }
     
     fileprivate struct ParseKeys {
-        static let Configuration                = "configuration"
-        static let Currency_api_key             = "currency_api_key"
-        static let Available_currencies         = "available_currencies"
-        static let Products                     = "products"
-        static let SourceCurrency               = "sourceCurrency"
+        static let Configuration        = "configuration"
+        static let Currency_api_key     = "currency_api_key"
+        static let Available_currencies = "available_currencies"
+        static let Products             = "products"
+        static let SourceCurrency       = "sourceCurrency"
     }
     
-    
+    // ==========================================================================
+    // MARK: - Methods
+    // ==========================================================================
     
     func loadAppConfiguration(completion: @escaping (ConfigResult<Any?>) -> Void ) {
         if let path = Bundle.main.path(forResource: "AppConfig", ofType: "json") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
                 
-                
                 if let jsonResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Dictionary<String, Any> {
                     
                     // Load products from configurationFile
                     if let rawProducts = jsonResult[ParseKeys.Products] as? Array<Dictionary<String, Any>> {
-                       // CoreDataStack.sharedInstance.clearProductData()
-                        
-                        
                         CoreDataStack.sharedInstance.createUpdateOrDeleteProducts(with: rawProducts)
                     }
                     
                     if let rawConfig = jsonResult[ParseKeys.Configuration] as? Dictionary<String, Any> {
                         
                         if let apiKey = rawConfig[ParseKeys.Currency_api_key] as? String {
-                            CurrencyApiManager.sharedInstance.apiKey = apiKey
+                            CurrencyManager.sharedInstance.apiKey = apiKey
                         }
                         
                         if let availableCurrencies = rawConfig[ParseKeys.Available_currencies] as? Array<String> {
-                            CurrencyApiManager.sharedInstance.availableCurrencies = availableCurrencies
+                            CurrencyManager.sharedInstance.availableCurrencies = availableCurrencies
                         }
                         
                         if let sourceCurrency = rawConfig[ParseKeys.SourceCurrency] as? String {
-                            CurrencyApiManager.sharedInstance.sourceCurrency = sourceCurrency
+                            CurrencyManager.sharedInstance.sourceCurrency = sourceCurrency
                         }
                         
                         completion(.Success(nil))
@@ -85,22 +83,11 @@ class AppConfigurationManager: NSObject {
             }
             catch let error {
                 print(error.localizedDescription)
+                completion(.Error(error))
             }
         }
         else {
             print("Invalid filename/path.")
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
